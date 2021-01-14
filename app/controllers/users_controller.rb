87808152b1bf06a6a642281a41692a646e2013e8
Kaitlin Regan <karegan@gmail.com>
2021-01-14 17:16:01 -0500

    Fixed sign up issue. Users show page now renders after signing up.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+    def index
+        
+    end
+
     def new
         @user = User.new
     end
@@ -7,21 +11,16 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.valid?
-            session[:user_id] = @user.id
             @user.save
-            redirect_to user_path(user)
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
         else
             render 'new'
         end
     end
 
     def show
-        if current_user
-            @user = User.find_by(id: params[:id])
-        else
-            redirect_to root_path
-        end
-
+        @user = User.find_by(id: params[:id])
     end
 
     private
